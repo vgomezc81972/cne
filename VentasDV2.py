5fa8d0d06d00d0ad8b67050d12dac391dc0293c3
Vitotoju <148645919+Vitotoju@users.noreply.github.com>
2023-10-27 20:28:21 -0500

    Add files via upload

--- VentasDV2.py
+++ VentasDV2.py
@@ -252,11 +252,10 @@ with st.container():
     ).properties(width=1200, height=1000)
     st.altair_chart(chart)
 with st.expander("Análisis"):
-    st.write("1. Se evidencia que el año 2008, tuvo mas ventas en todas las regiones\n"
-             "2. Se Evidencia que la decada los 80 fueron las ventas mas bajas\n"
-             "3. La Region que mas ventas tuvo fue NA\n"
-             "4. La Region que menos ventas tuvo fue Otros")
-
+    st.write("1. Se evidencia que la editorial : Nipo Amuse es la de mayor ventas\n"
+             "2. La segunda editorial de mayor ventas es : Edia\n"
+             "3. La editorial de menos ventas es : Hal Laboratory\n"
+             "4. La Region que mas ventas tuvo de editorial es : Ventas NA")
 
 with st.container():
     st.write("---")
@@ -272,8 +271,8 @@ with st.container():
     ).properties(width=800, height=400)
     st.altair_chart(chart)
 with st.expander("Análisis"):
-    st.write("1. Se evidencia que la editorial : Nipo Amuse es la de mayor ventas\n"
-             "2. La segunda editorial de mayor ventas es : Edia\n"
-             "3. La editorial de menos ventas es : Hal Laboratory\n"
-             "4. La Region que mas ventas tuvo de editorial es : Ventas NA")
+    st.write("1. Se evidencia que el año 2008, tuvo mas ventas en todas las regiones\n"
+             "2. Se Evidencia que la decada los 80 fueron las ventas mas bajas\n"
+             "3. La Region que mas ventas tuvo fue NA\n"
+             "4. La Region que menos ventas tuvo fue Otros")
 # %%
