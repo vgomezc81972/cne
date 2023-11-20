c193dc789380a928704bbe2fab2137d1953e9910
Vitotoju <148645919+Vitotoju@users.noreply.github.com>
2023-11-20 14:12:56 -0500

    Add files via upload

--- TiemposUrgFinal.py
+++ TiemposUrgFinal.py
@@ -229,6 +229,36 @@ with st.container():
         # Muestra la figura en Streamlit
         st.pyplot(fig)
 
+    with right_column:
+        st.header("MES")
+        st.write("Esta imagen muestra Por Timepo x Mes")
+    
+        # Ahora puedes acceder al día de la semana usando el atributo 'dayofweek'
+        df['Mes1'] = df[mask]['FECHA_LLEGADA'].dt.month
+
+        promedio = df[mask]['Tiempo_Minutos_Total'].median()
+        df[mask].loc[df[mask]['Tiempo_Minutos_Total'] > 420, 'Tiempo_Minutos_Total'] = promedio
+        df[mask].loc[df[mask]['Tiempo_Minutos_Total'] < 0, 'Tiempo_Minutos_Total'] = promedio
+
+        # Calcula el promedio de las predicciones para cada día de la semana
+        average_predicted_minutes = df.groupby('Mes1')['Tiempo_Minutos_Total'].mean()
+
+        # Trazar el gráfico de barras
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.barplot(x=average_predicted_minutes.index, y=average_predicted_minutes.values, color='#00FF00', ax=ax)
+        ax.set_xlabel('Mes')
+        ax.set_ylabel('Promedio del Tiempo (minutos)')
+        ax.set_title('Promedio del Tiempo en Horas')
+
+        # Añade etiquetas a las barras
+        for i, bar in enumerate(ax.patches):
+            yval = bar.get_height()
+            xval = bar.get_x() + bar.get_width() / 2
+            ax.text(xval, yval, f"{round(yval, 2)}", ha='center', va='bottom')
+
+        # Muestra la figura en Streamlit
+        st.pyplot(fig)        
+
 with st.container():
     st.write("---")
     st.header("Predicción de Tiempo de Espera de Atencion de Pacientes")
